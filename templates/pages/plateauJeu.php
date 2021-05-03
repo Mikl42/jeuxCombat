f8bfba3e1abf541c7e353e184bb8e660cefbf2c3
avhelia <83588077+avhelia@users.noreply.github.com>
2021-05-03 15:03:39 +0200

    Add files via upload

--- templates/pages/plateauJeu.php
+++ templates/pages/plateauJeu.php
@@ -1,116 +1,83 @@
 <?php
-
 /*
  * template plateauJeu:  amont afficher_plateauJeu.php
  * 
- * parametre recu :
- *          - objet player (toutes les caracteristiques du player)
- *          - listePlayer : Une liste de tous les joueurs qui sont dans la meme piece
+ * parametre recu : obejet player (toutes les caracteristiques du player)
  * 
  */
-
 ?>
 
 <!DOCTYPE html>
 
 <html>
 
-<head>
-    <?php include "templates/fragments/head.php"; ?>
-    <title>plateau du jeu Combat - Ecran principal - créer By Aurore, Mick et Will</title>
-</head>
+    <head>
+        <?php include "templates/fragments/head.php"; ?>
+        <title>plateau du jeu Combat - Ecran principal - créer By Aurore, Mick et Will</title>
+    </head>
 
-<body class="plateauJeu">
-    <main>
-        <h1>jeu de combat</h1>
-        <!-- zone player et adversaire -->
-        <div class="flex justify-around lg-95 first-zone" >
-            
-            <!-- zone player --> 
-            <div class="lg-18 flex justify-between zoneplayer">
-                <div class=" bg-primary player lg-100">
-                    <h2>personnage</h2>
-                    <div class="flex justify-around" >
-                        <p>pv</p>
-                        <span><?= $player->get("hp") ?></span>
-                    </div>
-                    <div class="flex justify-around" >
-                        <p>Force</p>
-                        <span class="strength"><?=  $player->get("strength") ?></span>
-                    </div>
-                    <div class="flex justify-around" >
-                        <p>agilité</p>
-                        <span class="agility"><?= $player->get("agility")?></span>
-                    </div>
-                    <div class="flex justify-around" >
-                        <p>resistance</p>
-                        <span class="resistance"><?= $player->get("resistance") ?></span>
-                    </div>
-                    <p><button onclick="augmenterStrength(<?= $player->getId()?>)" class="btn">augmenter Force</button></p>
-                    <p><button onclick="augmenterRes(<?= $player->getId()?>)"  class="btn">augmenter resistance</button></p>
+    <body class="plateauJeu">
+        <main>
+            <h1>jeu de combat</h1>
 
+            <!-- zone player et adversaire --> 
+
+
+            <div class="flex justify-around lg-95 first-zone" >
+
+                <!-- zone player --> 
+                <div class="lg-18 flex justify-between zoneplayer">
+                    <?php include "templates/fragments/player.php"; ?>
+
+                    <!-- zone adversaire --> 
+                    <div class = " bg-primary lg-100 adversaire">
+                        <h2>liste des adversaires</h2>
+                        <?php include "templates/fragments/adversaire.php"; ?>
+                    </div>
                 </div>
-                
-                
-                <!-- zone adversaire --> 
-                
-                
-                <div class=" bg-primary lg-100 adversaire">
-                    <h2>liste des adversaires</h2>
-                    <!--  $otherPlayer //foreach chaque adversaire present ds la salle afficher les pseudo  -->
-                    <div>
-                        <?php
-                        foreach ($listePlayer as $adverssaire){
-                            echo "<p><a href='".$adverssaire->getId()."'> ".$adverssaire->get('pseudo')." </a></p>";
-                        }
 
 
-                        ?>
+                <!-- zone SALLE de JEU --> 
+
+                <div class="lg-78 flex justify-around bg-primary zoneRoom">
+                    <div class="lg-10 border">
+                        <h2>entrée</h2>
+                        <?php include "templates/fragments/entryRoom.php"; ?>
                     </div>
+                    <div class="lg-75 border">
+                        <h2>salle <?= $player->get("room") ?></h2>
+                        <?php include "templates/fragments/roomPrincipal.php"; ?>
+                    </div>
+                    <div class="lg-10 border">
+                        <h2>sortie</h2>
+                        <?php include "templates/fragments/exitRoom.php"; ?>
+                    </div>
+                    <div class="flex justify-around lg-100 bouttonRoom">
+                        <p><button onclick="previousRoom()" class="btn previousRoom">reculer</button></p>
+                        <p><button onclick="nextRoom()" class="btn nextRoom">avancer</button></p>
 
-
+                    </div>
                 </div>
             </div>
-            
-            
-            <!-- zone SALLE de JEU --> 
-            
-            <div class="lg-78 flex justify-around bg-primary zoneRoom">
-                <div class="lg-10 border">
-                    <h2>entrée</h2>
-                </div>
-                <div class="lg-75 border">
-                    <h2>salle <!-- $room --> recuperation du numero de la salle courante ?></h2>
-                </div>
-                <div class="lg-10 border">
-                    <h2>sortie</h2>
-                </div>
-                <div class="flex justify-around lg-100 bouttonRoom">
-                    <p><button class="btn">reculer</button></p>
-                    <p><button class="btn">avancer</button></p>
-                    
-                </div>
-            </div>
-        </div>
-        
-        
-        
-        <!-- zone CONSOLE et MESSAGE -->      
-        
-        <div class="lg-100 bg-primary flex justify-between console">
-            <div class="lg-70 border">
-                <h2>console : </h2>
-                <p class="console"></p>
-            </div>
-            <div class="lg-28">
-                <h2>messages : </h2>
-                <p class="message"></p> 
-            </div>
-            
-        </div>
-    </main>
 
-</body>
-<?php include "templates/fragments/footer.php"; ?>
+
+
+            <!-- zone CONSOLE et MESSAGE -->      
+
+            <div class="lg-100 bg-primary flex justify-between console">
+                <div class="lg-70 border">
+                    <h2>console : </h2>
+                    <?php include "templates/fragments/console.php"; ?>
+                </div>
+                <div class="lg-28">
+                    <h2>messages : </h2>                    
+                    <?php include "templates/fragments/message.php"; ?>
+                </div>
+
+            </div>
+        </main>
+
+    </body>
+    <?php include "templates/fragments/footer.php"; ?>
 
 </html>
