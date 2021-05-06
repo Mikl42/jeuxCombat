495db06cc2cc5bc14383fdfe23b9a126803a28a6
avhelia <83588077+avhelia@users.noreply.github.com>
2021-05-06 09:50:26 +0200

    Add files via upload

--- js/fonction.js
+++ js/fonction.js
@@ -15,34 +15,55 @@
 // si success alors on modifie le template plateauJeu
 //error si un pbl est survenu durant le procedé
 
-//-------------------      augmenter STAT ----------------------
+$(function () {
+    setInterval(timerInterval, 500);
 
-function augmenterStrength(id) {
-    //role : declanche la modification des caracteristique et l'affichage sur le template 
+
+    function timerInterval() {
+        $.ajax("actualiser_plateauJeu.php", {
+            methode: "GET",
+            success: actuPlateau,
+            error: function () {
+                alert("Une erreur s'est produite.... again");
+            }
+        });
+    }
+
+    function actuPlateau(data) {
+        var plateau = $(".putaindeMain");
+        plateau.html(data);
+    }
+
+});
+
+
+function augmenterStrength() {
+    //role : modification de la force et l'affichage sur le template 
     //parametre :
     //  id : id du player
     //retour : affichage  success ou error
 
 
-    $.ajax("augmenter_force.php?id=" + id, {
+    $.ajax("augmenter_force.php", {
         methode: "GET",
         success: changeStat,
         error: function () {
-            alert("Une erreur s'est produite");
+            alert("Une erreur s'est produite force");
         }
     });
+
 }
-function augmenterRes(id) {
+function augmenterRes() {
     //role : declanche la modification des caracteristique et l'affichage sur le template 
     //parametre :
     //  id : id du player
 
 
-    $.ajax("augmenter_res.php?id=" + id, {
+    $.ajax("augmenter_res.php", {
         methode: "GET",
         success: changeStat,
         error: function () {
-            alert("Une erreur s'est produite");
+            alert("Une erreur s'est produite resistance");
         }
     });
 }
@@ -52,31 +73,59 @@ function changeStat(data) {
     //recup la zones a modifié
     var stat = $(".player");
 //ajoute le nouveau contenu dans les zones
-        stat.html(data);
-    
+    stat.html(data);
+
 }
 
-//----------------------       bouton reculer   -----------------------------
 
-function previousRoom(id){
+//----------------FUNCTION avancer ou reculer d'une piece------------------
+//-------------------------------------------------------------------------
+
+function previousRoom() {
     //role :declanche la modifi de l'affichage du fragments tableauJeu
     //parametre : id du player
     //return : success ou error
-    $.ajax("reculer.php?id=" + id, {
+    $.ajax("reculer.php", {
         methode: "GET",
         success: backToPreviousRoom,
         error: function () {
-            alert("Une erreur s'est produite");
+            alert("Une erreur s'est produite room previous");
         }
     });
-    
+
 }
 
-function backToPreviousRoom(data){
+function backToPreviousRoom(data) {
     //role : recupere la zone de modification
     //parametre data : donnée a modifier
-    
-    var room =$(".room");
+
+    var room = $(".roomPrincipal");
+    room.html(data);
+
+}
+
+function nextRoom() {
+    //role :declanche la modifi de l'affichage du fragments tableauJeu
+    //parametre : id du player
+    //return : success ou error
+    $.ajax("avancer.php", {
+        methode: "GET",
+        success: goToNextRoom,
+        error: function () {
+            alert("Une erreur s'est produite room next");
+        }
+    });
+
+}
+
+function goToNextRoom(data) {
+    var room = $(".roomPrincipal");
     room.html(data);
     
 }
+
+
+
+
+
+
